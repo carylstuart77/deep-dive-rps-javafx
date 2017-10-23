6fd9c393926288188d60486ddf7b2eb26d0794ba
Caryl Stuart <carylstuart@email.com>
2017-10-23 16:04:28 -0600

    --

---
+++ src/edu/cnm/deepdive/ca/rps/views/Timer.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive.ca.rps.views;
+
+import javafx.animation.AnimationTimer;
+
+public class Timer extends AnimationTimer {
+
+  private TerrainView view;
+
+  public Timer(TerrainView view) {
+    this.view = view;
+  }
+
+  @Override
+  public void handle(long now) {
+    view.draw();
+
+  }
+
+}
+
